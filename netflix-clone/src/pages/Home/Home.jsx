f2905916bf17024f88ea6e764fbd5b1197c2e340
yidne-ggitconfig --global user.email yidnebanketa1234.gmail.com <yidnebanketa1234@gmail.com>
2024-09-05 22:07:45 -0700

    rows

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import Banner from '../../components/Banner/Banner';
+import RowList from '../../components/Rows/RowList/RowList';
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       
       <Header/>
       <Banner/>
+      <RowList/>
       <Footer/>
     </>
   )
