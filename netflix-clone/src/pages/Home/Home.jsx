045d69d6a303bb1364f6e5f6c22b8b7159a08709
yidne-ggitconfig --global user.email yidnebanketa1234.gmail.com <yidnebanketa1234@gmail.com>
2024-09-05 20:26:58 -0700

    banner

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import Banner from '../../components/Banner/Banner';
 
 function Home() {
   return (
     <>
       
       <Header/>
+      <Banner/>
       <Footer/>
     </>
   )
