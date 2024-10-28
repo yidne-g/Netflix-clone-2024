6123482aacf885ebed6ba1ee196346ccc4dbef59
yidne-ggitconfig --global user.email yidnebanketa1234.gmail.com <yidnebanketa1234@gmail.com>
2024-10-28 15:30:55 -0700

    updated package json

--- netflix-clone/src/components/Header/Header.jsx
+++ netflix-clone/src/components/Header/Header.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
 import "./header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import NetflixLogo from "../../assets/images/NetflixLogo.png"
-// API-Key 42b00ddda818970b9975f24136a0c6c0
+import NetflixLogo from "../../assets/images/NetflixLogo.png";
 
- function Header() {
+function Header() {
   return (
-    <div className='header_outer_container'>
-      <div className='header_container'>
-        <div className='header_left'>
+    <div className="header_outer_container">
+      <div className="header_container">
+        <div className="header_left">
           <ul>
-            <li><img src={NetflixLogo} alt="Netflix Logo" Width="100"/></li>
+            <li>
+              <img src={NetflixLogo} alt="Netflix Logo" Width="100" />
+            </li>
             <li>Netflix</li>
             <li>Home</li>
             <li>TvShows</li>
@@ -23,12 +24,20 @@ import NetflixLogo from "../../assets/images/NetflixLogo.png"
             <li>Browse By Language</li>
           </ul>
         </div>
-        <div className='header_right'>
+        <div className="header_right">
           <ul>
-            <li><SearchIcon/></li>
-            <li><NotificationsIcon/></li>
-            <li><AccountBoxIcon/></li>
-            <li><ArrowDropDownIcon/></li>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
           </ul>
         </div>
       </div>
--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Netflix-clone-2024/",
+  base: "/Netflix-clone/",
   plugins: [react()],
-})
+});
